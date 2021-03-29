b5c21bbf11f26c83a6128fbcbfa6d7032e910f15
manjuchandramathi <manjuchandramathi@localhost>
2021-03-29 13:15:01 -0500

    new commit

--- src/main/java/com/example/config/JobConfig.java
+++ src/main/java/com/example/config/JobConfig.java
@@ -1,14 +1,23 @@
 package com.example.config;
 
 
+import java.sql.Date;
+
+import javax.batch.api.chunk.ItemProcessor;
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersIncrementer;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
@@ -18,8 +27,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import com.example.job.incrmentor.SampleIncrementer;
 import com.example.mapper.CustomerFieldSetMapper;
+import com.example.mapper.CustomerRowMapper;
 import com.example.model.Customer;
+import com.example.model.CustomerNew;
+import com.example.processor.CustomerProcessor;
+import com.example.processor.ManagerApprovalAppliactionProcessor;
+import com.example.reader.ManagerApprovalAppliactionReader;
+import com.example.writer.ManagerApprovalApplicationWriter;
 
 @Configuration
 public class JobConfig {
@@ -64,19 +80,103 @@ public class JobConfig {
 		return writer;
 	}
 	
+	
+	@Bean
+	@StepScope
+	public JdbcCursorItemReader<CustomerNew> itemReader() {
+		return new JdbcCursorItemReaderBuilder<CustomerNew>()
+				.dataSource(this.dataSource)
+				.name("creditReader")
+				.sql("select id,firstName, lastName from Customer_new")
+				.rowMapper(new CustomerRowMapper())
+				.build();
+
+	}
+	
+	@Bean
+	@StepScope
+	public CustomerProcessor processor() {
+	    return new CustomerProcessor();
+	}
+	
+	@Bean
+	@StepScope
+	public JdbcBatchItemWriter<CustomerNew> itemWriter(){
+		JdbcBatchItemWriter<CustomerNew> writer = new JdbcBatchItemWriter<>();
+		writer.setDataSource(this.dataSource);
+		writer.setSql("INSERT INTO Customer_Updated VALUES (:id, :firstName, :lastName)");
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+		writer.afterPropertiesSet();
+		
+		return writer;
+	}
+	
 	@Bean
 	public Step step1() {
 		return stepBuilderFactory.get("step1")
+				.<CustomerNew, CustomerNew> chunk(1)
+				.reader(itemReader())
+				.processor(processor())
+				.writer(itemWriter())
+				.build();
+	}
+	
+	@Bean
+	public Step step2() {
+		return stepBuilderFactory.get("step2")
 				.<Customer, Customer> chunk(1000)
 				.reader(customerItemReader())
 				.writer(customerItemWriter())
 				.build();
 	}
 	
+	
+	@Bean
+	@StepScope
+	public ManagerApprovalAppliactionProcessor ManagerProcessor() {
+	    return new ManagerApprovalAppliactionProcessor();
+	}
+	
+	@Bean
+	@StepScope
+	public ManagerApprovalAppliactionReader ManagerReader() {
+	    return new ManagerApprovalAppliactionReader();
+	}
+	
+	@Bean
+	@StepScope
+	public ManagerApprovalApplicationWriter ManagerWriter() {
+	    return new ManagerApprovalApplicationWriter();
+	}
+	
+
+	
+	@Bean
+	public Step step3() {
+		return stepBuilderFactory.get("step3")
+				.<CustomerNew, CustomerNew> chunk(1)
+				.reader(ManagerReader())
+				.processor(ManagerProcessor())
+				.writer(ManagerWriter())
+				.build();
+	}
+	
+	public JobParametersIncrementer jobParametersIncrementer() {
+	    return new SampleIncrementer();
+	}
+
+	public JobBuilder getJobBuilder(String jobName) {
+	    return jobBuilderFactory.get(jobName)
+	            .incrementer(jobParametersIncrementer());
+	}
+	
+	
 	@Bean
 	public Job job() {
-		return jobBuilderFactory.get("job")
-				.start(step1())
+		String jobane= "job"+System.currentTimeMillis();
+		return getJobBuilder(jobane)
+				.incrementer(new RunIdIncrementer())
+				.start(step3())
 				.build();
 	}	
 }
